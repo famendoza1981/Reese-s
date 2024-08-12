8c3f0dbd7cb7127755a2b9928c29fd33d38ca274
Fernando Mendoza <famendoza@gmail.com>
2024-08-11 22:05:49 -0400

    Made all the changes for the exercise 2 for Chapter 2.8

--- Reese's/ViewController.swift
+++ Reese's/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var messageLabel: UILabel!
+    let message1:String = "You Got Peanut Butter in my Chocolate!"
+    let message2:String = "You Got Chocolate in my Peanut Butter!"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        messageLabel.font = .boldSystemFont(ofSize: 40.0)
+        messageLabel.lineBreakMode = .byWordWrapping
+        messageLabel.numberOfLines = 10
     }
 
 
+    @IBAction func heyPressed(_ sender: UIButton) {
+        if message1 == messageLabel.text {
+            messageLabel.text = message2
+            messageLabel.textColor = .orange
+            messageLabel.textAlignment = .right
+        } else {
+            messageLabel.text = message1
+            messageLabel.textColor = .brown
+            messageLabel.textAlignment = .left
+        }
+    }
 }
 
